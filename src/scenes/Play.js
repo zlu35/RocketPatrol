20ce2c3b943ec336acb7156f126f3680bce2e3ff
Zhifeng <zlu35@ucsc.edu>
2020-04-13 21:42:27 -0700

    add asserts

---
+++ src/scenes/Play.js
@@ -0,0 +1,22 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+    
+    create() {
+        // white rectangle borders
+        this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        // green UI background
+        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+    }
+}
